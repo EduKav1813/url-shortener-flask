451d825e2ce607f605c1f84d8f777a8f474b4640
Eduard Kaverinskyi <KaverinskyiEduard1813@gmail.com>
2024-07-01 15:17:53 +0200

    Attach register/ endpoint to frontend

--- backend/routes/register.py
+++ backend/routes/register.py
@@ -6,7 +6,8 @@ bp = Blueprint("register", __name__, url_prefix="/register")
 
 @bp.route("/", methods=["POST"])
 def register_url():
-    url = request.args.get("url")
+    data = request.json
+    url = data["url"]
     if url:
         new_url = create_url(url)
         return jsonify({"url": new_url.original_url, "code": new_url.code})
